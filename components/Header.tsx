5b0a0073d5ea5a899e7f1d60b9f4e7d327d710f3
moaradc <143486936+moaradc@users.noreply.github.com>
2026-01-20 20:18:20 +0800

    Refactor Header component with new imports and methods

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,8 +1,30 @@
-import { Container, Group, Image } from '@mantine/core'
-import classes from '@/styles/Header.module.css'
-import { pageConfig } from '@/uptime.config'
-import { PageConfigLink } from '@/types/config'
+import Head from 'next/head'
+import { Inter } from 'next/font/google'
+import { MonitorTarget } from '@/types/config'
+import { maintenances, pageConfig, workerConfig } from '@/uptime.config'
+import OverallStatus from '@/components/OverallStatus'
+import Header from '@/components/Header'
+import MonitorList from '@/components/MonitorList'
+import { Center, Text } from '@mantine/core'
+import MonitorDetail from '@/components/MonitorDetail'
+import Footer from '@/components/Footer'
 import { useTranslation } from 'react-i18next'
+import { CompactedMonitorStateWrapper, getFromStore } from '@/worker/src/store'
+import { useRouter } from 'next/router'
+import { useEffect } from 'react'
+
+if (typeof Uint8Array !== 'undefined' && !Uint8Array.fromHex) {
+  Uint8Array.fromHex = function (string: string) {
+    if (string.length % 2 !== 0) {
+      throw new SyntaxError('Hex string must have an even length');
+    }
+    const bytes = new Uint8Array(string.length / 2);
+    for (let i = 0; i < string.length; i += 2) {
+      bytes[i / 2] = parseInt(string.substring(i, i + 2), 16);
+    }
+    return bytes;
+  };
+}
 
 export default function Header({ style }: { style?: React.CSSProperties }) {
   const { t } = useTranslation('common')
